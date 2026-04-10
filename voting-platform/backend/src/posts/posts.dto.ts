81f974fc0d29209fe7bbfd8b42a274ff106bcdbb
Wendreo Matheus <wendreo.pvh@gmail.com>
2026-04-10 18:44:39 -0300

    fix class-validator

--- voting-platform/backend/src/posts/posts.dto.ts
+++ voting-platform/backend/src/posts/posts.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
   title!: string;
+
+  @IsString()
+  @IsNotEmpty()
   description!: string;
 }
 
